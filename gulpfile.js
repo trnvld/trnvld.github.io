9784f24175d9324a3b20acca0552d890f0574bfe
Vlad Terniuk <vlad.terniu@gmail.com>
2019-09-10 20:17:07 +0300

    Addisng styles files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 'use strict';
+ 
 var gulp = require('gulp');
-var sass = require(‘gulp-sass’);
-compile gulp.task('sass', function () { gulp.src('scss/*.scss') .pipe(sass().on('error', sass.logError)) .pipe(gulp.dest('css')); });
-compile and watch gulp.task('sass:watch', function() { gulp.watch('app/scss/app.scss', ['sass']);});
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
