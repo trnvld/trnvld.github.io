a9f46aa3b54fc0afc8045c96e883c9d1d1dc1825
Vlad Terniuk <vlad.terniu@gmail.com>
2019-09-29 23:06:47 +0300

    Contact form added

--- js/main.js
+++ js/main.js
@@ -71,7 +71,129 @@ function fullscreenVideo() {
     document.addEventListener('keydown', modalClose);
 }
 
+
+
+function contactClick() {
+    var errors = false;
+    if (document.getElementById('form-name').value === '') {
+        $('#form-name-label').text('Please enter your full name');
+        $('#form-name').css('border-bottom-color', '#FF4F1A');
+        // $('#form-name').css('color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-comp').value === '') {
+        $('#form-comp-label').text('Please enter your company');
+        $('#form-comp').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-email').value === '') {
+        $('#form-email-label').text('Please enter your email');
+        $('#form-email').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-phone').value === '') {
+        $('#form-phone-label').text('Please enter your phone');
+        $('#form-phone').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-country').value === '') {
+        $('#form-country-label').text('Please enter your country');
+        $('#form-country').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (errors) {
+        $('#form-btn-contact').css('border-color', '#FF4F1A');
+    }
+    if (!errors) {
+        $('#contactModal .wrapper').addClass('animated fadeOut');
+        $('#contactModal .modal-login-heading').addClass('animated fadeOut');
+
+        var target = '.form-contact-msg';
+        $(target).css('visibility', 'hidden');
+        $('#form-btn-contact').css('display', 'none');
+        $(target).removeClass('animated');
+        $(target).removeClass('fadeInUp');
+        setTimeout(function () {
+            $('#contactModal .wrapper').css('display', 'none');
+            $('#contactModal .modal-login-heading').css('display', 'none');
+            $(target).css('visibility', 'visible');
+            $(target).addClass('animated fadeInUp');
+
+            $.ajax({
+                type: "POST",
+                url: "/mail.php",
+                data: $('form').serialize()
+            });
+        }, 1500);
+    }
+};
+
+function contactClickRu() {
+    var errors = false;
+    if (document.getElementById('form-name').value === '') {
+        $('#form-name-label').text('Пожалуйста, введите полное имя');
+        $('#form-name').css('border-bottom-color', '#FF4F1A');
+        // $('#form-name').css('color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-comp').value === '') {
+        $('#form-comp-label').text('Пожалуйста, введите компанию');
+        $('#form-comp').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-email').value === '') {
+        $('#form-email-label').text('Пожалуйста, введите email');
+        $('#form-email').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-phone').value === '') {
+        $('#form-phone-label').text('Пожалуйста, введите телефон');
+        $('#form-phone').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (document.getElementById('form-country').value === '') {
+        $('#form-country-label').text('Пожалуйста, введите страну');
+        $('#form-country').css('border-bottom-color', '#FF4F1A');
+        errors = true;
+    }
+    if (errors) {
+        $('#form-btn-contact').css('border-color', '#FF4F1A');
+    }
+    if (!errors) {
+        $('#contactModal .wrapper').addClass('animated fadeOut');
+        $('#contactModal .modal-login-heading').addClass('animated fadeOut');
+
+        var target = '.form-contact-msg';
+        $(target).css('visibility', 'hidden');
+        $('#form-btn-contact').css('display', 'none');
+        $(target).removeClass('animated');
+        $(target).removeClass('fadeInUp');
+        setTimeout(function () {
+            $('#contactModal .wrapper').css('display', 'none');
+            $('#contactModal .modal-login-heading').css('display', 'none');
+            $(target).css('visibility', 'visible');
+            $(target).addClass('animated fadeInUp');
+
+            $.ajax({
+                type: "POST",
+                url: "/mail.php",
+                data: $('form').serialize()
+            });
+        }, 1500);
+    }
+};
+
+
+
 $(document).ready(function () {
+    $(".contact-modal-link").animatedModal({
+        color: 'rgb(6,6,6)',
+        modalTarget: 'contactModal',
+        animatedIn: 'fadeIn',
+        animatedOut: 'fadeOut',
+        animationDuration: '1.5s'
+    });
+
     menuOffset();
     slideDownArrow();
     langChange();
