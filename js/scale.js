445f070e588fa32953acd72882d81b4ee9a8dbc1
Vlad Terniuk <vlad.terniu@gmail.com>
2019-10-23 22:55:47 +0300

    Fixing fade-in effects

--- js/scale.js
+++ js/scale.js
@@ -7,15 +7,18 @@ let blockBg = document.querySelector('.block__bg');
 function setActiveClass () {
     let pageScrollY = window.scrollY;
     let changeOpacity = pageScrollY * 0.001;
-    let changeScale =  1 + pageScrollY * 0.001;
+    let changeScale =  1 + pageScrollY * 0.003;
 
     blockElem.style.backgroundColor = `rgba(0,0,0,${changeOpacity})`;
     blockBg.style.transform = `scale(${changeScale})`;
 
     let changeLetterSpacing = pageScrollY * 0.08;
-    let changeOpacityPar = 1 - (changeOpacity / 1.2);
+    let changeOpacityPar = 1 - (changeOpacity / 0.8);
     if (changeOpacityPar > 0) {
         paragraph.style.opacity = `${changeOpacityPar}`;
+    }
+    else if (changeOpacityPar < 0.1) {
+        paragraph.style.opacity = 0;
     }
     paragraph.style.letterSpacing = `${changeLetterSpacing}px`;
 }
