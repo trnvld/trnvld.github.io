1ff6c10641604b868fc442a6dbdb921eccfe2ea7
v.terniuk <v.ternyuk@firelink.media>
2019-10-24 11:36:14 +0300

    Second fixing scale animation duration

--- js/scale.js
+++ js/scale.js
@@ -6,14 +6,14 @@ let blockBg = document.querySelector('.block__bg');
 
 function setActiveClass () {
     let pageScrollY = window.scrollY;
-    let changeOpacity = pageScrollY * 0.001;
-    let changeScale =  1 + pageScrollY * 0.005;
+    let changeOpacity = pageScrollY * 0.005;
+    let changeScale =  1 + pageScrollY * 0.001;
 
     blockElem.style.backgroundColor = `rgba(0,0,0,${changeOpacity})`;
     blockBg.style.transform = `scale(${changeScale})`;
 
     let changeLetterSpacing = pageScrollY * 0.2;
-    let changeOpacityPar = 1 - (changeOpacity / 0.7);
+    let changeOpacityPar = 1 - (changeOpacity / 0.8);
     if (changeOpacityPar > 0) {
         paragraph.style.opacity = `${changeOpacityPar}`;
     }
