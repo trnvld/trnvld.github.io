05a2ae89305b4b6725b7ddd61cd0e47ebf04d111
v.terniuk <v.ternyuk@firelink.media>
2019-12-18 13:24:55 +0200

    Adding custom cursor

--- js/main.js
+++ js/main.js
@@ -164,6 +164,36 @@ function contactClickRu() {
     }
 };
 
+$(document)
+    .mousemove(function(e) {
+        $('.your-cursor4')
+            .eq(0)
+            .css({
+                left: e.pageX,
+                top: e.pageY
+            });
+        setTimeout(function() {
+            $('.your-cursor4')
+                .eq(1)
+                .css({
+                    left: e.pageX,
+                    top: e.pageY
+                });
+        }, 100);
+        setTimeout(function() {
+            $('.your-cursor4')
+                .eq(2)
+                .css({
+                    left: e.pageX,
+                    top: e.pageY
+                });
+        }, 200);
+    });
+$(document).on("mousemove", function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
+
 
 
 $(document).ready(function() {
