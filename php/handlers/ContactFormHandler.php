07ed49477e7e8aa8bc621bc56fa805993eb5001d
Vlad Terniuk <vlad.terniuk@gmail.com>
2021-05-31 11:38:56 +0300

    Fixing contact and subscribe forms

--- php/handlers/ContactFormHandler.php
+++ php/handlers/ContactFormHandler.php
@@ -1,19 +1,46 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+require '../../vendor/autoload.php';
+include '../../envpath.php';
+
+$path = getcwd();
+echo $path;
+$dotenv = Dotenv\Dotenv::createImmutable($envpath);
+$dotenv->safeLoad();
+
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Encoding = 'base64';
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = $_ENV['SENDER_EMAIL'];
+$mail->Password = $_ENV['SENDER_PASSWORD'];
+$mail->SMTPKeepAlive = true;
+$mail->SMTPAuth = true;
+
+$mail->setFrom('support@blackshield.capital', 'BlackShield Capital');
+$mail->Subject = 'Новый контакт';
+
+$receiver = ['email' => $_ENV['RECEIVER_EMAIL'], 'name' => 'BlackShield Capital'];
+
+
+$mail->addAddress($receiver['email'], $receiver['name']);
+
 if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['company']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['country'])) {
-    $to = 'info@blackshield.fund';
-    $from = "support@blackshield.capital";
-    $subject = 'Новый контакт';
-    $message = "
-        Имя: $_POST[name]
-        Фамилия: $_POST[surname]
-        Компания: $_POST[company]
-        Почта: $_POST[email]
-        Телефон: $_POST[phone]
-        Страна: $_POST[country]
-        ";
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "From: " . $from . "\r\n";
-    $headers .= "Reply-To: " . $from . "\r\n";
-    mail($to, $subject, $message, $headers);
+  $mail->Body = "<p>Имя: {$_POST['name']}</p><p>Фамилия: {$_POST['surname']}</p><p>Компания: {$_POST['company']}</p><p>Почта: {$_POST['email']}</p><p>Телефон: {$_POST['phone']}</p><p>Страна: {$_POST['country']}</p>";
+        $mail->AltBody = "Имя: $_POST[name] \n Фамилия: $_POST[surname] \n Компания: $_POST[company] \n Почта: $_POST[email] \n Телефон: $_POST[phone] \n Страна: $_POST[country]";
+        echo json_encode(array('success' => true));
+        $mail->send();
+} else {
+  echo json_encode(array('success' => false));
 }
+
+$mail->clearAddresses();
+
+
+$mail->smtpClose();
 ?>
+
